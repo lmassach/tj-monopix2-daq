26dabbfac52d00e747185fc85158a44060e8f198
LabB2 <ludovico.massaccesi@desy.de>
2022-10-28 11:22:53 +0200

    ENH Load TDAC values from h5 file in scan_source

--- tjmonopix2/scans/scan_source.py
+++ tjmonopix2/scans/scan_source.py
@@ -7,6 +7,8 @@
 
 import time
 
+import numpy as np
+import tables as tb
 from tqdm import tqdm
 
 from tjmonopix2.system.scan_base import ScanBase
@@ -19,6 +21,8 @@ scan_configuration = {
     'stop_row': 220,
 
     'scan_time': 5,  # seconds
+    'load_tdac_from': None,  # Optional h5 file to load the TDAC values from
+    # 'load_tdac_from': "/home/labb2/tj-monopix2-daq/tjmonopix2/scans/output_data/module_0/chip_0/20221027_175016_local_threshold_tuning.h5",  # Optional h5 file to load the TDAC values from
 }
 
 register_overrides = {
@@ -38,41 +42,70 @@ registers = ['IBIAS', 'ICASN', 'IDB', 'ITUNE', 'ITHR', 'ICOMP', 'IDEL', 'VRESET'
 class SourceScan(ScanBase):
     scan_id = 'source_scan'
 
-    def _configure(self, start_column=0, stop_column=512, start_row=0, stop_row=512, **_):
+    def _configure(self, start_column=0, stop_column=512, start_row=0, stop_row=512, load_tdac_from=None, **_):
         self.chip.masks['enable'][:,:] = False
         self.chip.masks['injection'][:,:] = False
         self.chip.masks['enable'][start_column:stop_column, start_row:stop_row] = True
         self.chip.masks['tdac'][start_column:stop_column, start_row:stop_row] = 0b100
 
-        #Masking noisy pixels (W14R12 chip, HV FEs)
-        #self.chip.masks['tdac'][483, 379] = 0b000
-        #self.chip.masks['tdac'][485, 352] = 0b000
-        #self.chip.masks['tdac'][452, 248] = 0b000
-        #self.chip.masks['tdac'][480, 317] = 0b000
-        #self.chip.masks['tdac'][465, 302] = 0b000
-        #self.chip.masks['tdac'][461, 126] = 0b000
-        #self.chip.masks['tdac'][489, 510] = 0b000
-        #self.chip.masks['tdac'][483, 340] = 0b000
-        #self.chip.masks['tdac'][491, 450] = 0b000
-        #self.chip.masks['tdac'][494, 463] = 0b000
-        #self.chip.masks['tdac'][486, 498] = 0b000
-        #self.chip.masks['tdac'][476, 24] = 0b000
-        #self.chip.masks['tdac'][493, 242] = 0b000
-        #self.chip.masks['tdac'][474, 53] = 0b000
-        #self.chip.masks['tdac'][476, 211] = 0b000
-        #self.chip.masks['tdac'][487, 16] = 0b000
+        # Load TDAC from h5 file (optional)
+        if load_tdac_from:
+            with tb.open_file(load_tdac_from) as f:
+                file_tdac = f.root.configuration_out.chip.masks.tdac[:]
+                file_tdac = file_tdac[start_column:stop_column, start_row:stop_row]
+                # Do not replace TDAC values with zeros from the file, use the default for those pixels
+                self.chip.masks['tdac'][start_column:stop_column, start_row:stop_row] = \
+                    np.where(
+                        file_tdac != 0, file_tdac,
+                        self.chip.masks['tdac'][start_column:stop_column, start_row:stop_row])
 
-        #self.chip.masks['hitor'][0, 0] = True
+        # Disable W8R13 bad/broken columns (25, 160, 161, 224, 274, 383-414 included, 447) and pixels
+        self.chip.masks['enable'][25,:] = False  # Many pixels don't fire
+        self.chip.masks['enable'][160:162,:] = False  # Wrong/random ToT
+        self.chip.masks['enable'][224,:] = False  # Many pixels don't fire
+        self.chip.masks['enable'][274,:] = False  # Many pixels don't fire
+        self.chip.masks['enable'][383:415,:] = False  # Wrong/random ToT
+        self.chip.masks['enable'][447,:] = False  # Many pixels don't fire
+        self.chip.masks['enable'][75,159] = False
+        self.chip.masks['enable'][163,219] = False
+        self.chip.masks['enable'][427,259] = False
+        self.chip.masks['enable'][219,161] = False # disable hottest pixel on chip
+
+        # # Noisy/hot W8R13 pixels
+        # for col, row in [(219, 161), (222, 188), (219, 192), (219, 129), (221, 125), (219, 190), (220, 205), (220, 144), (220, 168), (219, 179), (221, 136), (222, 186), (219, 163), (221, 205), (226, 135), (222, 174), (221, 199), (222, 185), (221, 203), (225, 181), (220, 123), (222, 142), (223, 143), (220, 154), (221, 149), (221, 179), (222, 120), (219, 125)] \
+        #         + [(227, 167), (222, 177), (221, 156), (221, 196), (219, 170), (221, 150), (222, 172), (221, 132), (219, 133), (226, 146), (220, 187), (219, 200), (227, 142), (220, 173), (229, 142), (219, 131), (219, 195), (221, 126), (220, 136), (222, 159), (221, 188), (227, 207), (219, 167), (219, 193), (220, 131), (228, 158), (219, 198), (221, 139), (219, 211), (221, 177), (223, 189), (219, 156), (222, 144), (222, 184), (228, 200), (219, 174), (229, 197), (220, 184), (222, 133), (222, 171), (219, 152), (222, 147), (222, 197), (219, 132), (223, 167), (221, 163), (223, 126), (227, 199), (229, 168), (229, 188), (221, 165), (222, 139), (219, 217), (222, 161), (225, 143), (219, 187), (221, 202), (223, 141), (222, 134), (220, 204), (221, 209), (220, 191), (221, 183), (223, 137), (223, 188), (221, 219), (222, 140), (223, 146), (219, 142), (222, 219), (229, 134), (221, 168), (227, 145), (222, 199), (227, 211), (220, 201), (220, 217), (221, 190), (221, 216), (223, 204), (221, 186), (227, 178), (222, 125), (221, 122), (220, 190), (227, 196), (222, 141), (220, 145), (229, 215)] \
+        #         + [(221, 174), (219, 188)] \
+        #         + [(228, 203), (219, 180), (227, 137), (220, 146), (219, 146), (221, 137), (221, 172)] \
+        #         + [(228, 181), (221, 211), (228, 201), (221, 123), (229, 173), (220, 143), (228, 219), (220, 135), (226, 141), (222, 217), (228, 188), (228, 156), (222, 122), (229, 191), (222, 160), (225, 146), (227, 155), (220, 179), (220, 175), (219, 137), (220, 149), (227, 171), (226, 178), (221, 200), (220, 198), (222, 181), (221, 198), (228, 216), (227, 174), (228, 133), (219, 182), (229, 149), (219, 136), (229, 163), (223, 148), (221, 120), (221, 160), (222, 213), (219, 175), (225, 203), (222, 165), (221, 170), (225, 186), (228, 190), (221, 143), (219, 194), (222, 194), (219, 124), (226, 133), (223, 199), (221, 192), (219, 205), (219, 139), (225, 207), (225, 197), (221, 169), (219, 153), (219, 186), (221, 148), (221, 131), (219, 173), (228, 169), (228, 150), (219, 191), (221, 129), (222, 193), (222, 196), (220, 186), (220, 148), (221, 189), (221, 134), (219, 135), (220, 209), (221, 159), (220, 182), (220, 169), (222, 215), (221, 171), (220, 121), (228, 209), (219, 123), (220, 153), (222, 201), (220, 210), (221, 151), (219, 207), (222, 162), (227, 132), (220, 203), (228, 198), (228, 144), (221, 140), (222, 192), (223, 217), (219, 196), (226, 199), (223, 177), (225, 193), (219, 181), (222, 178), (219, 144), (221, 164), (219, 171), (219, 201), (220, 125), (219, 130), (222, 207)] \
+        #         :
+        #     self.chip.masks['enable'][col,row] = False
+
+        # W8R13 pixels that fire even when disabled
+        # For these ones, we disable the readout of the whole double-column
+        reg_values = [0xffff] * 16
+        for col in [85, 109, 131, 145, 157, 163, 204, 205, 279, 282, 295, 327, 335]:
+            dcol = col // 2
+            reg_values[dcol//16] &= ~(1 << (dcol % 16))
+        for i, v in enumerate(reg_values):
+            # EN_RO_CONF
+            self.chip._write_register(155+i, v)
+            # EN_BCID_CONF
+            self.chip._write_register(171+i, v)
+            # EN_RO_RST_CONF
+            self.chip._write_register(187+i, v)
+            # EN_FREEZE_CONF
+            self.chip._write_register(203+i, v)
 
         self.chip.masks.apply_disable_mask()
         self.chip.masks.update(force=True)
 
         self.chip.registers["SEL_PULSE_EXT_CONF"].write(0)
 
+        self.scan_registers = {}
         for r in self.register_overrides:
             if r != 'scan_time':
                 self.chip.registers[r].write(self.register_overrides[r])
-            #print("Write: ", r, " to ", self.register_overrides[r])
+                self.scan_registers[r] = self.register_overrides[r]
 
         self.daq.rx_channels['rx0']['DATA_DELAY'] = 14
 
@@ -88,11 +121,6 @@ class SourceScan(ScanBase):
                 now = time.time()
                 pbar.update(int(round(now - last_time)))
         pbar.close()
-
-        ret = {}
-        # for r in registers:
-        #     ret[r] = self.chip.registers[r].read()
-        self.scan_registers = ret
 
         self.log.success('Scan finished')
 
